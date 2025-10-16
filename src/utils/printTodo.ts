ecee8749a3b16e476fddb35ec89b9fe9f29a0fc9
yuzu <120536904+YuNi1005@users.noreply.github.com>
2025-10-16 13:46:55 +0900

    class02 section03

--- src/utils/printTodo.ts
+++ src/utils/printTodo.ts
@@ -4,6 +4,7 @@ import type { Todo } from "../types.js";
 export const printTodo = (todo: Todo): void => {
   const todoSummary =
     `(優先度: ${todo.priority}) ${todo.name}` +
-    ` 期日: ${dayjs(todo.deadline).format("YYYY/MM/DD HH:mm")}`;
+    ` 期日: ${dayjs(todo.deadline).format("YYYY/MM/DD HH:mm")}` +
+    ` 完了ステート: ${todo.isDone}`;
   console.log(todoSummary);
 };
